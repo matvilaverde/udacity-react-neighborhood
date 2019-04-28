8dc3f8c7ef34a7feb3b07e9697c220c90e5a4cb0
Mateus Vilaverde <mateusvilaverde@gmail.com>
2019-04-28 16:39:07 -0300

    Siderbar with show/hide markers buttons

--- src/App.js
+++ src/App.js
@@ -16,13 +16,25 @@ class App extends Component {
     window.initMap = this.initMap
   }
 
+  showListings = () => {
+    for(var i=0; i<this.state.markers.length; i++) {
+      this.state.markers[i].setMap(this.state.map);
+    }
+  }
+
+  hideListings = () => {
+    for(var i=0; i<this.state.markers.length; i++) {
+      this.state.markers[i].setMap(null);
+    }
+  }
+
   initMap = () => {
-    let map = new window.google.maps.Map(document.getElementById('map'), {
+    this.setState({map: new window.google.maps.Map(document.getElementById('map'), {
       center:{lat: -19.9408184, lng: -43.9355109},
       zoom: 16,
       styles:[{"featureType":"all","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels","stylers":[{"visibility":"off"},{"saturation":"-100"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40},{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"landscape","elementType":"geometry.stroke","stylers":[{"color":"#4d6059"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"lightness":21}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"color":"#4d6059"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#7f8d89"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#2b3638"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2b3638"},{"lightness":17}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"geometry.stroke","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.icon","stylers":[{"visibility":"off"}]}],
       
-    })
+    })})
 
     let infowindow = new window.google.maps.InfoWindow();
 
@@ -33,21 +45,31 @@ class App extends Component {
 
       let marker = new window.google.maps.Marker({
         position: position,
-        map: map,
-        title: title
+        title: title,
+        animation: window.google.maps.Animation.DROP,
+        id: i
       })
 
       let contentString = `${this.locations[i].title}`
 
       marker.addListener('click', function() {
         infowindow.setContent(contentString)
-        infowindow.open(map, marker)
+        infowindow.open(this.state.map, marker)
       })
       
       this.state.markers.push(marker);
-      console.log(marker.title);
+
+      document.getElementById('show-listings').addEventListener('click', this.showListings);
+      document.getElementById('hide-listings').addEventListener('click', this.hideListings);
     }
 
+
+    /*
+      var bounds = new window.google.maps.LatLngBounds();
+      bounds.extend(marker.position);
+
+      map.fitBounds(bounds);
+    */
   }
 
   locations = [
@@ -77,7 +99,16 @@ class App extends Component {
   render() {
     return (
       <main>
-        <div id="map"></div>
+        <div className="container">
+          <div className="options-box">
+            <h1>Find Rock Bars Near You!</h1>
+            <div>
+              <input id="show-listings" type="button" value="Show Listings" onClick={this.showMarkers}/>
+              <input id="hide-listings" type="button" value="Hide Listings" onClick={this.hideMarkers} />
+            </div>
+          </div>
+          <div id="map"></div>
+        </div>
       </main>
     );
   }
@@ -86,7 +117,7 @@ class App extends Component {
 function loadScript(url) {
   var index = window.document.getElementsByTagName('script')[0]; // Selects first script tag
   var script = window.document.createElement('script'); // Creates script tag
-  script.src = url;
+  script.src = url; // Inserts the source property using the argument
   script.async = true; // Inserts the async property
   script.defer = true; // Inserts the defer property
   index.parentNode.insertBefore(script, index); // Keeps script at the very beggining
