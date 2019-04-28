41ceff83ff82c85abcdbf4000b05993a56a76808
Mateus Vilaverde <mateusvilaverde@gmail.com>
2019-04-28 12:07:20 -0300

    Working script tag and map; initial README

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    this.renderMap();
+  }
+
+  renderMap = () => {
+    loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAOFG5MfWrnTNAlxiYoko5BOd6s5RTHrhY&callback=initMap')
+    window.initMap = this.initMap
+  }
+
+  initMap = () => {
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    })
+  }
+
+  render() {
+    return (
+      <main>
+        <div id="map"></div>
+      </main>
+    );
+  }
+}
+
+function loadScript(url) {
+  var index = window.document.getElementsByTagName('script')[0]; // Selects first script tag
+  var script = window.document.createElement('script'); // Creates script tag
+  script.src = url;
+  script.async = true; // Inserts the async property
+  script.defer = true; // Inserts the defer property
+  index.parentNode.insertBefore(script, index); // Keeps script at the very beggining
 }
 
 export default App;
