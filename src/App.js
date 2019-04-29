a2ea20e62a685eeb6ae9972d60099b5650fe2ea3
Mateus Vilaverde <mateusvilaverde@gmail.com>
2019-04-29 17:51:58 -0300

    Removed map from state; set bounds

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
 
   showListings = () => {
     for(var i=0; i<this.state.markers.length; i++) {
-      this.state.markers[i].setMap(this.state.map);
+      this.state.markers[i].setMap(this.map);
     }
   }
 
@@ -28,16 +28,19 @@ class App extends Component {
     }
   }
 
+  map;
+
   initMap = () => {
-    this.setState({map: new window.google.maps.Map(document.getElementById('map'), {
+    this.map = new window.google.maps.Map(document.getElementById('map'), {
       center:{lat: -19.9408184, lng: -43.9355109},
       zoom: 16,
       styles:[{"featureType":"all","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels","stylers":[{"visibility":"off"},{"saturation":"-100"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40},{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"landscape","elementType":"geometry.stroke","stylers":[{"color":"#4d6059"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"lightness":21}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"color":"#4d6059"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#7f8d89"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#2b3638"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2b3638"},{"lightness":17}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"geometry.stroke","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.icon","stylers":[{"visibility":"off"}]}],
       
-    })})
+    })
 
     let infowindow = new window.google.maps.InfoWindow();
 
+    let bounds = new window.google.maps.LatLngBounds();
 
     for(let i=0; i<this.locations.length; i++) {
       let position = {lat: this.locations[i].position.lat, lng: this.locations[i].position.lng} 
@@ -54,15 +57,20 @@ class App extends Component {
 
       marker.addListener('click', function() {
         infowindow.setContent(contentString)
-        infowindow.open(this.state.map, marker)
+        infowindow.open(this.map, marker)
       })
       
       this.state.markers.push(marker);
 
-      document.getElementById('show-listings').addEventListener('click', this.showListings);
-      document.getElementById('hide-listings').addEventListener('click', this.hideListings);
-    }
+      
+      bounds.extend(marker.position);
 
+      this.map.fitBounds(bounds);
+
+    }
+    
+    document.getElementById('show-listings').addEventListener('click', this.showListings);
+    document.getElementById('hide-listings').addEventListener('click', this.hideListings);
 
     /*
       var bounds = new window.google.maps.LatLngBounds();
